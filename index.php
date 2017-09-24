1c8e4f2d48a1c88ae33f74161da5f3141eaa3683
dodosiz <asvestopouli@hotmail.com>
2017-09-25 00:24:22 +0300

    Adding static files

---
+++ index.php
@@ -0,0 +1,80 @@
+﻿<!DOCTYPE html>
+<html lang="el">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        
+        <!-- Bootstrap core CSS -->
+    	<link href="./vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    	
+        <link rel="stylesheet" href="./css/app.css">
+    </head>
+    <body>
+    
+    	<!-- The navigation bar -->
+    	<nav id="menu" class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <a class="navbar-brand" href="#">Brand Name</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" href="#intro">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#product">Products <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#contact">Contact <span class="sr-only">(current)</span></a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        
+        <!-- The intro section -->
+    	<div id="intro" class="container-fluid">
+    		<div class="content">
+            	<h1 class="display-4">Welcome</h1>
+            	<p class="lead">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                </p>
+            </div>
+        </div>
+        
+        <!-- The product section -->
+        <div id="product" class="container-fluid">
+        	<h2>We Offer</h2>
+    		<div class="row">
+    			<div class="col-lg-3 col-md-4 col-sm-6">
+    				<img src="./img/bread.png" class="img-fluid" alt="bread">
+    			</div>
+            	<div class="col-lg-3 col-md-4 col-sm-6">
+            		<img src="./img/pie.png" class="img-fluid" alt="pie">
+    			</div>
+    			<div class="col-lg-3 col-md-4 col-sm-6">
+    				<img src="./img/sweet.png" class="img-fluid" alt="sweet">
+    			</div>
+    			<div class="col-lg-3 col-md-4 col-sm-6">
+    				<img src="./img/milk.png" class="img-fluid" alt="drink">
+    			</div>
+    			<div class="col-lg-3 col-md-4 col-sm-6">
+    				<img src="./img/drink.png" class="img-fluid" alt="drink">
+    			</div>
+            </div>
+        </div>
+        
+        <!-- The contact section -->
+        <div id="contact" class="container-fluid">
+        	<h2>Contact</h2>
+        </div>
+    
+    	<!-- Bootstrap core JavaScript -->
+        <script src="./vendor/jquery/jquery.min.js"></script>
+        <script src="./vendor/popper/popper.min.js"></script>
+        <script src="./vendor/bootstrap/js/bootstrap.min.js"></script>
+        
+        <script src="./js/app.js"></script>
+    </body>
+</html>
